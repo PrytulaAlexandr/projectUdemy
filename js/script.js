39d5952ceb746df11ac2bd78549be5b75e4c8a3a
Alexandr <liga5872@gmail.com>
2022-02-04 09:59:33 +0200

    add script about film

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
-	count : numberOfFilms,
-	movies : {},
-	actors : {},
-	genders : [],
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
 	privat: false,
 };
 
-const lastWatchFilm = prompt('Один из последних просмотренных фильмов?', '');
-const markOfFilm = prompt('На сколько оцените его?', '');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', ''),
+		c = prompt('Один из последних просмотренных фильмов?', ''),
+		d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
